7c6fd69a307509437001c1fc61c418a6f750cb5c
brain <shahriarmehedi95@gmail.com>
2021-12-17 12:13:06 +0600

    store

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Test;
 
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function index()
+    {
+        $test=Test::all();
+        return $test;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/alldata',[TestController::class,'index']);
+
+Route::post('/add-student',[TestController::class,'store']);

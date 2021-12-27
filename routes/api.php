d6c8566ee8884976bd40c0e1c4f4dee18bfd10a7
brain <shahriarmehedi95@gmail.com>
2021-12-28 00:27:33 +0600

    login

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\User\AuthController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+ /////////////// User Login API Start ////////////////////////
+
+ // Login Routes 
+Route::post('/login',[AuthController::class, 'Login']);
+
+ // Register Routes 
+Route::post('/register',[AuthController::class, 'Register']);
+
+ // Current User Route 
+Route::get('/user',[UserController::class, 'User'])->middleware('auth:api');
+ /////////////// End User Login API Start ////////////////////////
 
 
 

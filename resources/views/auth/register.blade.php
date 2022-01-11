5abace8c6d7c20cd83e80c78911b8fdfb2a3961d
brain <shahriarmehedi95@gmail.com>
2022-01-11 17:58:58 +0600

    shahriar done for today

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--favicon-->
-    <link rel="icon" href="{{ asset('assets/images/favicon-32x32.png') }}" type="image/png" />
+    <link rel="icon" href="{{ asset('backend/assets/images/profile.png') }}" type="image/png" />
     <!--plugins-->
     <link href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
@@ -27,8 +27,8 @@
         <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
             <div class="container-fluid">
                 <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-                    <div class="col mx-auto">
-                        <div class="mb-4 text-center">
+                    <div class="col mx-auto ">
+                        <div class="mb-5 text-center mt-20">
                             <img src="{{ asset('backend/assets/images/logo.png') }}" width="180" alt="" />
                         </div>
                         <div class="card">

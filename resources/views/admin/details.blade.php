f2f94c82545a891b7d60f24ff9af2224624e6bc2
brain <shahriarmehedi95@gmail.com>
2022-01-29 11:01:10 +0600

    test

--- resources/views/admin/details.blade.php
+++ resources/views/admin/details.blade.php
@@ -32,7 +32,7 @@
               <th>#</th>
               <th>Date</th>
               <th>In Time</th>
-              <th>Late Reason</th>
+              <th>Late Reasons</th>
               <th>Status</th>
             </tr>
           </thead>

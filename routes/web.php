74c21520d45e79e55e11dfe669756b2d900f9864
brain <shahriarmehedi95@gmail.com>
2022-01-29 15:17:02 +0600

    d

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //all employee
 Route::get('/allemployee',[EmployeeController::class, 'index'])->name('allemployee');
 Route::get('/details/{id}',[EmployeeController::class, 'details']);
+Route::get('/allemployee',[EmployeeController::class, 'index'])->name('allemployee');
+Route::get('/details/{id}',[EmployeeController::class, 'details']);
